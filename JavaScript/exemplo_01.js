b7ee10f6ab5d694c672783bc2fc25acc969c89f0
Jeferson Tigik da Silva <80992765072@senacrs.edu.br>
2025-10-01 12:30:54 -0300

    Brand New Folder add

---
+++ JavaScript/exemplo_01.js
@@ -0,0 +1,19 @@
+// ===========================================================================
+// Exemplo laço de repetição
+
+let cont = 1;
+
+while (cont <= 5) {
+    console.log("While: "+cont);
+    cont++;
+}
+cont = 1;
+do {
+    console.log("Do-While: "+cont);
+    cont++;
+} while (cont <= 5);
+
+for (let i = 0; i < i < 10; i++) {
+    console.log("For: "+cont);
+    
+}
