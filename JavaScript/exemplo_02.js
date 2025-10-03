1cadc601d2ac6b85cfdff660335165aa18aacd11
Jeferson Tigik da Silva <80992765072@senacrs.edu.br>
2025-10-03 12:14:59 -0300

    2nd commit to repository

---
+++ JavaScript/exemplo_02.js
@@ -0,0 +1,14 @@
+//Declarar e inicializar a estrutura
+let numeros = [1,2,3];
+//Mostrar e adicionar no FIM
+console.log(numeros);
+numeros.push(6);
+console.log(numeros);
+//Mostrar e remover no INÍCIO
+numeros.shift(1);
+console.log(numeros);
+//Mostrar e adicionar no INÍCIO
+numeros.unshift(-3)
+console.log(numeros)
+//Imprimir um item específico
+console.log("Mostrando: "+ numeros[3])
