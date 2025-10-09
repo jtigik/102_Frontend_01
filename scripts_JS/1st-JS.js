584016c8521a52f91a17b9bb7eb0cc83354642e9
jtigik <jeftigik@hotmail.com>
2025-10-09 00:33:40 -0300

    Add a brand new Loop

--- scripts_JS/1st-JS.js
+++ scripts_JS/1st-JS.js
@@ -29,18 +29,55 @@ console.log(obj.nome); // "João"
 let novoJson = JSON.stringify(obj); // Converte objeto para string
 console.log(novoJson);
 
-let frutas = ["maçã", "banana", "laranja"]; // Como um vetor no Portugol
+let frutas = ['maçã', 'banana', 'laranja']; // Como um vetor no Portugol
 console.log(frutas[0]); // "maçã"
-frutas[1] = "morango"; // Modifica o item
+frutas[1] = 'morango'; // Modifica o item
 console.log(frutas);
-frutas.push("uva"); // Adiciona um item no final
+frutas.push('uva'); // Adiciona um item no final
 console.log(frutas);
 frutas.splice(1, 1); // Remove o item na posição 1
 console.log(frutas);
-frutas.push("banana"); // Adiciona "banana" novamente
-console.log(frutas)
+frutas.push('banana'); // Adiciona "banana" novamente
+console.log(frutas);
 
 let notas = [7, 8, 9];
 for (let i = 0; i < notas.length; i++) {
-console.log("Nota " + (i+1) + ": " + notas[i]);
+    console.log('Nota ' + (i + 1) + ': ' + notas[i]);
+}
+
+notas.forEach(function (nota) {
+    console.log(nota);
+});
+
+let soma = notas.reduce(function (total, nota) {
+    return total + nota;
+}, 0);
+
+console.log('Soma das notas: ' + soma);
+
+let aluno = {
+    nome: 'Ana',
+    idade: 16,
+    nota: 8.5,
+};
+console.log(aluno.nome); // "Ana"
+aluno.idade = 17; // Modifica
+console.log(aluno);
+
+//==============================================================
+
+let pessoa1 = {"nome": "Ana Silva", "idade": 17};
+let pessoa2 = {"nome": "Pedro", "idade": 14};
+let pessoa3 = {"nome": "Maria", "idade": 16};
+
+let pessoas = [pessoa1, pessoa2, pessoa3];
+
+console.log(pessoas);
+
+for (const index in pessoas) {
+    if (!Object.hasOwn(pessoas, index)) continue;
+    
+    const element = pessoas[index];
+    console.log("Nome: %s - Idade: %d", element.nome, element.idade);
+    
 }
